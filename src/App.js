fc36a3e34ee97caaa96ca6d628119849a6aa0448
HuberAdrian <huberadrian@web.de>
2022-11-16 20:00:00 +0100

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // make a homepage for a learning platform with a navbar, a sidebar, login and signup buttons, a footer and a main content area with a scrollable list of courses
+
+  // create a navbar component
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
