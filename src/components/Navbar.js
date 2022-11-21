f45ac28f62dace30c4599accba39e4c7f26d47e3
HuberAdrian <huberadrian@web.de>
2022-11-22 00:58:36 +0100

    added hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,91 @@
 import React from 'react'
-// import image 
-import gear from '.././images/gear.png'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
+import './Navbar.css'
+import { Button } from '../../globalStyles'
 
 function Navbar() {
-    // create a navbar component with a logo, a login button and a signup button
+  const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
 
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+     setButton(false)
+    } 
+    else {
+       setButton(true)
+    }
+  }
+
+  window.addEventListener('resize', showButton)
 
   return (
-
-    <div className='navbar' >
-        <div className='logo' >
-            <img src={gear} alt='logo' />
+   <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+       <div className='navbar'>
+         <div className='navbar-container container'>
+           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+             TRVL <i className='fab fa-typo3' />
+           </Link>
+           <div className='menu-icon' onClick={handleClick}>
+             {click ? <FaTimes /> : <FaBars />}
+           </div>
+           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+             <li className='nav-item'>
+               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                 Home
+               </Link>
+             </li>
+              <li className='nav-item'>
+               <Link
+                 to='/services'
+                 className='nav-links'
+                 onClick={closeMobileMenu}
+               >
+                 Services
+               </Link>
+              </li>
+              <li className='nav-item'>
+               <Link
+                 to='/products'
+                 className='nav-links'
+                 onClick={closeMobileMenu}
+               >
+                 Products
+               </Link>
+              </li>
+              <li className='nav-btn'>
+                {button ? (
+                 <Link to='/sign-up' className='btn-link'>
+                   <Button buttonStyle='btn--outline'>SIGN UP</Button>
+                 </Link>
+                 ) : (
+                   <Link
+                     to='/sign-up'
+                     className='btn-link'
+                     onClick={closeMobileMenu}
+                   >
+                     <Button
+                       buttonStyle='btn--outline'
+                       buttonSize='btn--mobile'
+                     >
+                       SIGN UP
+                     </Button>
+                   </Link>
+                 )}
+              </li>
+            </ul>
+          </div>
         </div>
+      </IconContext.Provider>
+    </>
+   )
+ }
 
-        <div className='links' >
-        <a href='home'>Startseite</a>
-        <a href='about'>Kurse</a>
-        <a href='Preise'>Preise</a>
-        <button className='login' >Login</button>
-        <button className='signup' >Registrieren</button>
-        </div>
-    </div>
-  )
-}
-
-export default Navbar
+ export default Navbar
+ 
