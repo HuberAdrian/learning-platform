0ef01c30d4b22fb7efb8fab046beff724544a2cb
HuberAdrian <huberadrian@web.de>
2022-11-23 01:09:26 +0100

    added files for landing

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import Footer from './components/Footer'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
 
 function Home() {
 
     // make a landing page for a mechanical engineering learning page with a button to login and a button to signup
-
-    
-
  
   return (
     <>
-    <Navbar/>
-    <div className='homepage'>Home</div>
-    <Footer/>
+      {/* <Navbar /> */}
+      <div className="home">
+
+      </div>
+      <Courses />
+      <Pricing />
+      <About />
+      <Footer />
     </>
   )
 } 
