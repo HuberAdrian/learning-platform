528bef02efb6044227cdb0e16cbafcd5362dd007
HuberAdrian <huberadrian@web.de>
2022-12-13 10:38:20 +0100

    added close button for testing

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Person from './components/Person';
 
 function App() {
-  // make a homepage for a learning platform with a navbar, a sidebar, login and signup buttons, a footer and a main content area with a scrollable list of courses
 
-  // create a navbar component
   return (
     <div className="App">
 
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Person() {
-    // make two buttons to test the functionality of the app either as a teacher or as a student
+    
+    const navigate = useNavigate();
+
+    // go back to the home page
+    const onClickClose = () => {
+        navigate('/');
+    }
 
     return (
         <>
+            <button onClick={onClickClose} className='test-close' >X</button>
             <button className='test-teacher' >Teacher</button>
             <button className='test-student' >Student</button>
         </>
