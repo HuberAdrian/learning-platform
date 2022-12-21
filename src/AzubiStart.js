fe6a44593d2395d7eac774a46ce5d575f74338a6
HuberAdrian <huberadrian@web.de>
2022-12-21 11:40:35 +0100

    added core parts to AzubiStart

--- src/AzubiStart.js
+++ src/AzubiStart.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
 function Azubi() {
+
+  // make a starting page for the Student (Azubi) where he can see his progress in a timeline and his current tasks with the recommended courses he can take next to improve his skills and knowledge
+  // the timeline should be animated and show the progress of the student from the start to the end of the college (maybe with a progress bar) 
+  // make also a field where he can join group games with other students to improve his skills and knowledge
+
   return (
-    <div>Azubi</div>
+    <div className='container-start' >
+      <h1>Startseite Azubi</h1>
+      <div className='container-timeline' >
+        <h2>Timeline</h2>
+      </div>
+      <div className='container-tasks' >
+        <h2>Anstehende Prüfungen</h2>
+      </div>
+      <div className='container-courses' >
+        <h2>Empfohlene Kurse</h2>
+      </div>
+      <div className='container-games' >
+        <h2>Gruppenspiele</h2>
+      </div>
+    </div>
   )
 }
 
