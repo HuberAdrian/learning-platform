42270a870452597328bc8257c4cb21c960f13f85
HuberAdrian <huberadrian@web.de>
2022-12-26 12:21:00 +0100

    fixed link bug in CoursePreview

--- src/components/CoursePreview.js
+++ src/components/CoursePreview.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function CoursePreview(props) {
 
-    const { navigate } = useNavigate();
     const { link } = props;
 
-    const onClickCourse = () => {
-        navigate(link);
-    }
-
   return (
-    <div className='azubi-courses' onClick={onClickCourse}  >
+    <Link to={link}>
+    <div className='azubi-courses'>
           <h3>Kurs 1</h3>
           <p>bla bla bla</p>
         </div>
+    </Link>
   )
 }
 
