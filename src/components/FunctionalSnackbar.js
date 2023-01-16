390fce66359b0307a5abcb14cdc2df7dcbec1431
HuberAdrian <huberadrian@web.de>
2023-01-16 18:56:28 +0100

    added snackbar component

---
+++ src/components/FunctionalSnackbar.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import "./snackbar.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+const FunctionalSnackbar = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <button onClick={handleClick}>Button not available</button>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        message="This button is not available yet. Please try again later."
+        action={
+          <React.Fragment>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+        className="snackbar"
+      />
+    </div>
+  );
+};
+
+export default FunctionalSnackbar;
